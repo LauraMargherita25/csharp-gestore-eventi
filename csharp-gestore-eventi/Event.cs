a7c7a320be838964b3790caceb8a6653e5139b12
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-07-06 17:26:30 +0200

    Class Event

---
+++ csharp-gestore-eventi/Event.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    internal class Event
+    {
+        private string title;
+        private DateTime date;
+        private int seats;
+        private int reservedSeats;
+
+
+        public Event (string title, DateTime date, int seats, int reservedSeats)
+        {
+            this.Title = title;
+            this.Date = date;
+            this.Seats = seats;
+            this.ReservedSeats = reservedSeats;
+            
+        }
+
+        public string Title {
+            get { return this.title; } 
+            set 
+            { 
+                this.title = value;
+
+                if (this.title == null)
+                {
+                    throw new ArgumentNullException("title");
+                }
+            } 
+        }
+        public DateTime Date {
+            get { return this.date; }
+            set { 
+                this.date = value;
+                
+                DateTime today = DateTime.Today;
+                int res = DateTime.Compare(value, today);
+                if (res < 0)
+                {
+                    throw new InvalidOperationException();
+                }
+            } 
+        }
+        public int Seats {
+            get { return this.seats } 
+            private set { 
+                this.seats = value;
+                
+                if(this.seats < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            } 
+        }
+        public int ReservedSeats { get; private set; } = 0;
+        
+    }
+}
--- csharp-gestore-eventi/Program.cs
+++ csharp-gestore-eventi/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// evento -> id
+//        -> data
+//        -> posti totali
+//        -> posti prenotati // lista di clienti prenotati?
+//        -> posti ancora disponibili
+//        -> concluso(se la data di oggi > data evento)
+
+     
+// Gestionale che gestisce gli eventi
+//--lista eventi
+//--()crea evento
+
+
+//cliente -> ()prenota --> evento.numero prenotazioni +1
+//        -> ()disdici --> evento.numero prenotazioni -1 ?? voglio evitaare che un cliente disdica senza aver mai prenotato 
+
+
